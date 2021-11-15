d469b652cc0c4b80f14a7be89739f8ac8fba6acb
hanani14 <fhanani841@gmail.com>
2021-11-15 22:25:22 +0800

    splitting the app into widget

--- fluttertraining/lib/main.dart
+++ fluttertraining/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertraining/answer.dart';
 import 'package:fluttertraining/question.dart';
+import 'package:fluttertraining/quiz.dart';
+import 'package:fluttertraining/result.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,10 +16,10 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   //the state can be persistent
-  var questionIndex = 0;
+  var _questionIndex = 0;
   // use a 'final' if a value doesn't change from the point of time when you program runs. It's runtime constant value but when we're writing code we don't know what the final value will be
   // use a 'const' if you know value doesn't change during runtime and when write the code
-  final questions = const [
+  final _questions = const [
     {
       'questionText': 'What\'s your fav food?',
       'answer': ['Black', 'Red', 'Green', 'White'],
@@ -34,15 +36,15 @@ class _MyAppState extends State<MyApp> {
 
   //questions = []; //does not work if questions is a const
 
-  void answerQuestion() {
+  void _answerQuestion() {
     // var aBool = true;
     // aBool = false;
 
     setState(() {
-      questionIndex += 1;
-      print(questionIndex);
+      _questionIndex += 1;
+      print(_questionIndex);
     });
-    if (questionIndex < questions.length) {
+    if (_questionIndex < _questions.length) {
       print('We have more questions!');
     } else {
       print('No more questions!');
@@ -56,34 +58,40 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(
             title: Text("My First Apps"),
           ),
-          body: questionIndex < questions.length
-              ? Column(
-                  children: [
-                    Question(questions[questionIndex]['questionText']),
-                    ...(questions[questionIndex]['answer'] as List<String>)
-                        .map((answer) {
-                      ///... - it take a list which is exactly what we have here and the pull all the value s in the list out of it and add them to the surrounding list as individual values
-                      return Answer(answerQuestion, answer);
-                    }).toList()
-                    // Answer(
-                    //     answerQuestion), //function also can be pass using pointer. Answer pass answerQuestion() and Answer will accept the parameter of funcion
-                    // Answer(answerQuestion),
-                    // Answer(answerQuestion),
-                    // RaisedButton(
-                    //   onPressed: () => print(
-                    //       'Answer 2 chosen!'), //anonymous functions because don't have name of function
-                    //   child: Text('Answer 2'),
-                    // ),
-                    // RaisedButton(
-                    //   onPressed: () {
-                    //     //anonymous functions because don't have name of function
-                    //     print('Answer 3 chosen!');
-                    //   },
-                    //   child: Text('Answer 3'),
-                    // ),
-                  ],
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions: _questions,
                 )
-              : Center(child: Text('You did it!'))),
+
+              // ? Column(
+              //     children: [
+              //       Question(questions[_questionIndex]['questionText']),
+              //       ...(questions[questionIndex]['answer'] as List<String>)
+              //           .map((answer) {
+              //         ///... - it take a list which is exactly what we have here and the pull all the value s in the list out of it and add them to the surrounding list as individual values
+              //         return Answer(answerQuestion, answer);
+              //       }).toList()
+              //       // Answer(
+              //       //     answerQuestion), //function also can be pass using pointer. Answer pass answerQuestion() and Answer will accept the parameter of funcion
+              //       // Answer(answerQuestion),
+              //       // Answer(answerQuestion),
+              //       // RaisedButton(
+              //       //   onPressed: () => print(
+              //       //       'Answer 2 chosen!'), //anonymous functions because don't have name of function
+              //       //   child: Text('Answer 2'),
+              //       // ),
+              //       // RaisedButton(
+              //       //   onPressed: () {
+              //       //     //anonymous functions because don't have name of function
+              //       //     print('Answer 3 chosen!');
+              //       //   },
+              //       //   child: Text('Answer 3'),
+              //       // ),
+              //     ],
+              //   )
+              : Result()),
     );
   }
 }
---
+++ fluttertraining/lib/quiz.dart
@@ -0,0 +1,29 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:fluttertraining/answer.dart';
+import 'package:fluttertraining/question.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions; //nested generic assigments
+  final Function answerQuestion;
+  final int questionIndex;
+
+  Quiz(
+      {@required this.questions,
+      @required this.answerQuestion,
+      @required this.questionIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(questions[questionIndex]['questionText']),
+        ...(questions[questionIndex]['answer'] as List<String>).map((answer) {
+          ///... - it take a list which is exactly what we have here and the pull all the value s in the list out of it and add them to the surrounding list as individual values
+          return Answer(answerQuestion, answer);
+        }).toList()
+      ],
+    );
+  }
+}
